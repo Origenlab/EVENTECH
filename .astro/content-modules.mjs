6e6167ddf99d23cca0ea0b5eb6708b30e2078f1c
Origenlab <Origenlab@users.noreply.github.com>
2026-02-26 14:05:24 -0600

    Initial commit

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/content/blog/segundo-articulo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fsegundo-articulo.mdx&astroContentModuleFlag=true")]]);
+		
